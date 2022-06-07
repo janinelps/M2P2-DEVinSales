1264e6bdb416748eaf139bfdeb166a529c267c5e
janinelps <janinelpss@gmail.com>
2022-06-07 23:36:57 +0100

    Implementação autenticação JWT

---
+++ DevInSales/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using DevInSales.Models;
+
+namespace DevInSales.Repositories
+{
+    public class UserRepository
+    {
+        public static User CheckNameAndPassword(string username, string password)
+        {
+            var users = new List<User>();
+
+            users.Add(new User() { Id = 1, Name = "João", Password = "123", Role = "funcionario"});
+            users.Add(new User() { Id = 2, Name = "Maria", Password = "123", Role = "gerente" });
+
+            return users.Where(x => x.Name.ToLower() == username.ToLower() && x.Password == password)
+            .FirstOrDefault();
+        }
+    }
+}
---
+++ DevInSales/Startup.cs
@@ -0,0 +1,61 @@
+﻿using NuGet.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DevInSales
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
